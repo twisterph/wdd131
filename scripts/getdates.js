fdf7550a8a654476ac9c58582f9b383bc7681a36
twisterph <phinojosaiturra@byupathway.edu>
2026-03-05 18:53:52 -0700

    marzo 5

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,2 @@
-
-const year = document.querySelector ("#currentyear");
-const modified = document.querySelector ("#lastmodified");
-
-if (year) {
-    year.textContent = new Date().getFullYear();
-}
-if (modified) {
-    modified.textContent = `Last Modified: ${document.lastModified}`;
-}
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
