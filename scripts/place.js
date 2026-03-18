07894cc387fcecbabd618703d54a9e7e5f70efd7
twisterph <phinojosaiturra@byupathway.edu>
2026-03-17 20:35:03 -0700

    w3-9

--- scripts/place.js
+++ scripts/place.js
@@ -1,26 +1,30 @@
-
-
 const currentYear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 const temperatureElement = document.querySelector("#temperature");
 const windSpeedElement = document.querySelector("#windspeed");
 const windChillElement = document.querySelector("#windchill");
+
 const temperature = 8;
 const windSpeed = 12;
-currentYear.textContent = new Date().getFullYear();
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
 function calculateWindChill(temp, speed) {
-  return (
-    13.12 +
-    0.6215 * temp -
-    11.37 * Math.pow(speed, 0.16) +
-    0.3965 * temp * Math.pow(speed, 0.16)
-  ).toFixed(1);
+  return (13.12 + 0.6215 * temp - 11.37 * speed ** 0.16 + 0.3965 * temp * speed ** 0.16).toFixed(1);
 }
-temperatureElement.textContent = temperature;
-windSpeedElement.textContent = windSpeed;
-if (temperature <= 10 && windSpeed > 4.8) {
-  windChillElement.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
-} else {
-  windChillElement.textContent = "N/A";
+if (currentYear) {
+  currentYear.textContent = new Date().getFullYear();
+}
+if (lastModified) {
+  lastModified.textContent = `Last Modified: ${document.lastModified}`;
+}
+if (temperatureElement) {
+  temperatureElement.textContent = temperature;
+}
+if (windSpeedElement) {
+  windSpeedElement.textContent = windSpeed;
+}
+if (windChillElement) {
+  if (temperature <= 10 && windSpeed > 4.8) {
+    windChillElement.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+  } else {
+    windChillElement.textContent = "N/A";
+  }
 }
