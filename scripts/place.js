a36a97122a9ed1fa6a5a1daf53fd4ae650917d26
twisterph <phinojosaiturra@byupathway.edu>
2026-03-17 19:52:27 -0700

    w3-6

--- scripts/place.js
+++ scripts/place.js
@@ -5,13 +5,10 @@ const lastModified = document.querySelector("#lastModified");
 const temperatureElement = document.querySelector("#temperature");
 const windSpeedElement = document.querySelector("#windspeed");
 const windChillElement = document.querySelector("#windchill");
-
 const temperature = 8;
 const windSpeed = 12;
-
 currentYear.textContent = new Date().getFullYear();
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
-
 function calculateWindChill(temp, speed) {
   return (
     13.12 +
@@ -20,10 +17,8 @@ function calculateWindChill(temp, speed) {
     0.3965 * temp * Math.pow(speed, 0.16)
   ).toFixed(1);
 }
-
 temperatureElement.textContent = temperature;
 windSpeedElement.textContent = windSpeed;
-
 if (temperature <= 10 && windSpeed > 4.8) {
   windChillElement.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
 } else {
